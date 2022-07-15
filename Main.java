d6364a86c822706bf11d8ad7509c45fb626517f0
vj6199 <vijaykrishna6199@gmail.com>
2022-07-15 00:07:03 -0400

    Create Main.java
    initial commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    static String name;
+    static int id;
+    static double salary;
+
+    public static void main(String[] Yashoda)
+     {
+        System.out.println("name :"+name);
+        System.out.println("id :"+id);
+        System.out.println("salary :"+salary);
+        System.out.println("hospital :"+Yashoda[0]);
+     }
+}
